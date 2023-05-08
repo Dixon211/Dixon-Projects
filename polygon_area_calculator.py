266064d2413834e56ede2cc3dfdd328688204462
Dixon211 <131546168+Dixon211@users.noreply.github.com>
2023-05-08 16:00:47 -0400

    Update polygon_area_calculator.py
    Setup classes that need to be done and started on some of the formulas

--- polygon_area_calculator.py
+++ polygon_area_calculator.py
@@ -1,1 +1,29 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+    
+    def __str__(self):
+        rect_print = f"Rectangle:(width={self.width}, height={self.height})"
+        return rect_print
+    
+    def set_width(self, width):
+        self.width = width
+    def set_height(self, height):
+        self.height = height
+    def get_area(self):
+        return self.width*self.height
+    def get_perimeter(self):
+        return 2*(self.width)+2*(self.height)
+    def get_diagonal(self):
+        return ((self.width ** 2 + self.height ** 2) ** .5)
+    def get_picture(self):
+        star_line = f"{'*'*self.width}\n"
+        star_lines = star_line * self.height
+        return star_lines
+    def get_amount_inside(self):
+        return None
 
+
+practice_rect = Rectangle(6, 8)
+print(practice_rect.get_picture())
