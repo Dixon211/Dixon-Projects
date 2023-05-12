de79b1637b076063005e4113cd59c37360476272
Dixon211 <131546168+Dixon211@users.noreply.github.com>
2023-05-12 07:53:34 -0400

    Update probability_calculator.py

--- probability_calculator.py
+++ probability_calculator.py
@@ -7,26 +7,25 @@ class Hat:
 
     def __str__(self):
         return str(self.balls)
-    # very happy about the if statement layer
+    
+    # very happy about the if statement layer, verified functionality
     def draw(self, draws):
         balls_instance = self.balls
         drawn_balls = {}
-        self.draws = draws
-        if self.draws > sum(balls_instance.value()):
-            while self.draws > 0:
-                drawn_ball = random.choice(list(balls_instance.keys()))
-                if drawn_ball in balls_instance and balls_instance[drawn_ball] > 0:
-                    balls_instance[drawn_ball] -= 1
-                    if drawn_ball in drawn_balls:
-                        drawn_balls[drawn_ball] += 1
-                    else:
-                        drawn_balls[drawn_ball] = 1
-                    self.draws -= 1
-                else:
-                    continue
-        else:
+        if draws >= sum(balls_instance.values()):
             return balls_instance
-        return print(drawn_balls)
+        while draws > 0:
+            drawn_ball = random.choice(list(balls_instance.keys()))
+            if drawn_ball in balls_instance and balls_instance[drawn_ball] > 0:
+                balls_instance[drawn_ball] -= 1
+                if drawn_ball in drawn_balls:
+                    drawn_balls[drawn_ball] += 1
+                else:
+                    drawn_balls[drawn_ball] = 1
+                draws -= 1
+            else:
+                continue
+        return drawn_balls
 
     
     
@@ -35,5 +34,5 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     return None
 
 test_hat = Hat(red=3, blue=2)
-print(test_hat)
-test_hat.draw(3)
+drawn_balls = test_hat.draw(3)
+print(str(drawn_balls))
