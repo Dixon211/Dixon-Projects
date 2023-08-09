5f71300e237fd78761ec7910c17922cc8431e49a
Dixon211 <131546168+Dixon211@users.noreply.github.com>
2023-08-08 21:54:33 -0400

    React website setup

---
+++ My_Website/tutorial info/Website_Tutorial/index.js
@@ -0,0 +1,9 @@
+//western, drama, scifi
+favoriteMovieGenre("regular")
+//watermelon, tomato, etc
+favoriteFruit("regular")
+//light, dark, round
+favoriteMode("light")
+//
+favoriteEdgeStyle("sharp")
+
---
+++ react_site/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import headshot from './Headshot.jpg';
+
+
+function App() {
+  return (
+    <div className='main'>
+      <div className='left'>
+        <img className='headshot' src={headshot}/>
+        
+      </div>
+      <div className='right'>
+
+      </div>
+    </div>
+);
+}
+
+export default App;
